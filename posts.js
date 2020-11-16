6ed9f0ce48f0cd573ddcc3180b44d76213f0a036
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-11-16 13:50:42 -0700

    async and await

---
+++ posts.js
@@ -0,0 +1,14 @@
+var posts = [
+{ 
+    title: "Mit",
+    content: "Great Place to Hang Out"
+},
+{
+    title: "Cricket",
+    content: "Takes a While To Master"
+},
+{
+    title: "Marmalade",
+    content: "Chunky is the best"
+}
+]
---
+++ script.js
@@ -0,0 +1,24 @@
+function makeRequest(location) { 
+    return new Promise((resolve, reject) => {
+        console.log(`Making request to ${location}`)
+        if (location==='Google') {
+            resolve('Google says Hi!')
+        } else {
+            reject('We can only talk to Google')
+        }
+    })
+}
+
+function processRequest(response) {
+    return new Promise((resolve, reject) => {
+        console.log('Processing response')
+        resolve(`Extra information + ${response}`);
+    })
+}
+
+makeRequest('Google').then(response => {
+    console.log('Response Received')
+    return processRequest(response)
+}).then(processedResponse => {
+    console.log(processedResponse)
+})
